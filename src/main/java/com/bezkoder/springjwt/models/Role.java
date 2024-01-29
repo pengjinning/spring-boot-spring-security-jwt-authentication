2f6ff92a80d51e954a523f46905aa625744e9961
jack ning <github@bytedesk.com>
2024-01-29 12:05:22 +0800

    update

--- src/main/java/com/bezkoder/springjwt/models/Role.java
+++ src/main/java/com/bezkoder/springjwt/models/Role.java
@@ -1,3 +1,11 @@
+/*
+ * @Author: jack ning github@bytedesk.com
+ * @Date: 2024-01-29 11:48:06
+ * @LastEditors: jack ning github@bytedesk.com
+ * @LastEditTime: 2024-01-29 11:57:50
+ * @FilePath: /spring-boot-spring-security-jwt-authentication/src/main/java/com/bezkoder/springjwt/models/Role.java
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package com.bezkoder.springjwt.models;
 
 import jakarta.persistence.*;
@@ -5,6 +13,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "roles")
 public class Role {
+  
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
